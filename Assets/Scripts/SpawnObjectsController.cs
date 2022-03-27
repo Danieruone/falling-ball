ea35186eed875bb2024c130f2207e6559f37b06c
Daniel <danieruone@gmail.com>
2022-03-26 22:34:58 -0500

    Add score logic and points prefab

--- Assets/Scripts/SpawnObjectsController.cs
+++ Assets/Scripts/SpawnObjectsController.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class SpawnObjectsController : MonoBehaviour
 {
     public Transform[] spawnPoints;
+    public Transform[] spawnObstacles;
+
+    public GameObject point;
     public GameObject obstacle;
-    // Start is called before the first frame update
+
     void Start()
     {
+        InvokeRepeating("LaunchPoint", 1.0f, 1.0f);
         InvokeRepeating("LaunchObstacle", 2.0f, 3.0f);
+    }
+
+    void LaunchPoint()
+    {
+        int randomNumber = Random.Range(0, 3);
+        Instantiate(point, spawnPoints[randomNumber].position, spawnPoints[randomNumber].rotation);
     }
 
     void LaunchObstacle()
     {
         int randomNumber = Random.Range(0, 2);
-        Instantiate(obstacle, spawnPoints[randomNumber].position, spawnPoints[randomNumber].rotation);
+        Instantiate(obstacle, spawnObstacles[randomNumber].position, spawnObstacles[randomNumber].rotation);
     }
 }
